90e978641e714508549bcba342508111a575a657
Gabriel Alao <gabrieldeveloper1@gmail.com>
2020-08-23 07:48:02 -0400

    vue frontend project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <HeaderHome />
+    <FiturBrand />
+    <PaketHosting />
+    <FiturPHP />
+    <FiturDua />
+    <FiturLaravel />
+    <Module />
+    <LinuxHosting />
+    <Sosmed />
+    <Bantuan />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import HeaderHome from "./components/HeaderHome.vue";
+import FiturBrand from "./components/FiturBrand.vue";
+import PaketHosting from "./components/PaketHosting.vue";
+import FiturPHP from "./components/FiturPHP.vue";
+import FiturDua from "./components/FiturDua.vue";
+import FiturLaravel from "./components/FiturLaravel.vue";
+import Module from "./components/Module.vue";
+import LinuxHosting from "./components/LinuxHosting.vue";
+import Sosmed from "./components/Sosmed.vue";
+import Bantuan from "./components/Bantuan.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    HeaderHome,
+    FiturBrand,
+    PaketHosting,
+    FiturPHP,
+    FiturDua,
+    FiturLaravel,
+    Module,
+    LinuxHosting,
+    Sosmed,
+    Bantuan,
+    Footer,
+  },
+};
+</script>
